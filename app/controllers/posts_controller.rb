0dcabb95903b1973bb3d78ed8f06b6427803cd96
MansoorZaman <manisureal@outlook.com>
2018-02-01 13:07:17 +0000

    Added show action to the categories controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,10 +26,6 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
-    # @categories_arr = Category.all.map do |c|
-    #    c.name
-    #    c.id
-    # end
   end
 
   def update
