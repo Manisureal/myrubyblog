1bc0b14f4e68680d42769a6f86c3e2b3cb3c7e32
MansoorZaman <manisureal@outlook.com>
2018-02-07 11:23:52 +0000

    Dashoard code has been implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   root 'home#index'
   resources :posts
   resources :categories
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
 end
