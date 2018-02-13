e6fb0c815708e101e1a44eec85d65486a7a35887
MansoorZaman <manisureal@outlook.com>
2018-02-13 16:04:45 +0000

    Created custom routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  get 'pages/resources'
-
+#Static Pages Routes
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'resources', to: 'pages#resources'
+#Default Homepage route
   root 'home#index'
+#All other controllers routes
   resources :posts
   resources :categories
   devise_for :admin_users, ActiveAdmin::Devise.config
